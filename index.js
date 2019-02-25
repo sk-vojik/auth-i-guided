8a54976ab319099822cd3cc3271738860b9989fd
Scott Vojik <scott.vojik@gmail.com>
2019-02-25 12:18:01 -0800

    demo complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-module.js');
@@ -18,8 +19,9 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
   // generate hash from user's password
-
+  const hash = bcrypt.hashSync(user.password, 14); // 2 ^ n, 2^14
   // override user.password with hash
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
@@ -37,7 +39,7 @@ server.post('/api/login', (req, res) => {
     .first()
     .then(user => {
       // check that passwords match
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -48,13 +50,47 @@ server.post('/api/login', (req, res) => {
     });
 });
 
+function restricted (req, res, next) {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        // check that passwords match
+        if (user && bcrypt.compareSync(password, user.password)) {
+            next()
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'No creds provided' });
+      });
+  } else {
+    res.status(400).json({ message: 'No creds provided' });
+  }
+
+}
+
 // protect this route, only authenticated users should see it
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
+});
+
+//ASYNC VERSION
+server.get('/users', restricted, async (req, res) => {
+  try {
+    const users = await Users.find();
+
+    res.json(users);
+  } catch (error) {
+    res.send(err)
+  }
 });
 
 const port = process.env.PORT || 5000;
